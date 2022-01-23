50c3fef4f3a5ebf03df983416633bc5fb56a6e2a
Marcin Dłubis <marcindl@hotmail.com>
2022-01-23 13:57:26 +0100

    promises - part IV - async&await

--- js/index.js
+++ js/index.js
@@ -58,33 +58,63 @@
 ////////////// zad 3 ///////////////
 ////////////////////////////////////
 
-
 // w pierwszym promisie zmienną globalną powiększamy o pięć (promise ma trwać 5 sekund)
 // w drugim promisie otrzymaną nową wartość (10) powiększamy o 20 (promise ma trwać 10 sekund)
 
-let news = 5;
+// let news = 5;
 
-const firstDelay = () => {
-    return new Promise( (resolve, reject) => {
-        setTimeout( () => {
-            news = news + 5;
-            resolve(news);
-        },5000)
+// const firstDelay = () => {
+//     return new Promise( (resolve, reject) => {
+//         setTimeout( () => {
+//             news = news + 5;
+//             resolve(news);
+//         },5000)
+//     })
+// }
+
+// const secondDelay = (fromFirstDelay) => {
+//     return new Promise( (resolve, reject) => {
+//         setTimeout( () => {
+//             fromFirstDelay = fromFirstDelay + 20;
+//             resolve(fromFirstDelay);
+//         },10000)
+//     })
+// }
+
+// const output = result => {
+//     console.log(result);
+// }
+
+// firstDelay()
+//     .then(secondDelay)
+//     .then(output)
+
+////////////////////////////////////
+////////////// zad 4 ///////////////
+////////////////////////////////////
+
+const news = 5;
+const myPromise = () => {
+    return new Promise(resolve => {
+        resolve(news);
     })
 }
-const secondDelay = (fromFirstDelay) => {
-    return new Promise( (resolve, reject) => {
-        setTimeout( () => {
-            fromFirstDelay = fromFirstDelay + 20;
-            resolve(fromFirstDelay);
-        },10000)
-    })
+const addFive = num => {
+    return num + 5;
+}
+const muplityBy = num => {
+    return num * 77;
 }
 
 const output = result => {
-    console.log(result);
+    console.log(result)
 }
 
-firstDelay()
-    .then(secondDelay)
-    .then(output)
+async function wywolanie() {
+    let zmienna1 = await myPromise();
+    let zmienna2 = addFive(zmienna1)
+    let zmienna3 = muplityBy(zmienna2)
+    output(zmienna3)
+}
+
+wywolanie();
